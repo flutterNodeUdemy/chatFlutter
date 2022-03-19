d1743ae4b3b08b4ed98f24ed45e580ffd6e4262d
oscarruiz86 <oscarruiz86@gmail.com>
2022-03-18 22:48:51 -0500

    commit chat

---
+++ lib/models/usuario.dart
@@ -0,0 +1,11 @@
+class Usuario {
+  bool online;
+  String email;
+  String nombre;
+  String uid;
+  Usuario(
+      {required this.email,
+      required this.nombre,
+      required this.online,
+      required this.uid});
+}
